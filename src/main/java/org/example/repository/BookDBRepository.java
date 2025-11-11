e11b12bb7de593e4c83e1378d428927a3e21e539
KoStickSipliy <esipov.kostya.20005@gmail.com>
2025-11-11 18:54:59 +0300

    created BookDBRepository, created constructor

--- src/main/java/org/example/repository/BookDBRepository.java
+++ src/main/java/org/example/repository/BookDBRepository.java
@@ -1,11 +1,37 @@
 package org.example.repository;
 
+import lombok.Getter;
+import org.example.DBManager.PostgreSQLManager;
+import org.example.IO.IO;
 import org.example.entities.Book;
 
+import java.sql.*;
 import java.util.Collection;
 import java.util.List;
 
 public class BookDBRepository implements BookRepository {
+    @Getter
+    private static final BookDBRepository instance = new BookDBRepository();
+
+    private Statement statement;
+    private PreparedStatement createStatement;
+    private PreparedStatement deleteByIdStatement;
+    private PreparedStatement updateStatement;
+    private PreparedStatement getByIdStatement;
+
+    private BookDBRepository () {
+        Connection connection = PostgreSQLManager.getInstance().getConnection();
+
+        try {
+            statement = connection.createStatement();
+            createStatement = connection.prepareStatement("INSERT INTO book (name, path) VALUES (?, ?)");
+            deleteByIdStatement = connection.prepareStatement("DELETE FROM book WHERE id = ?");
+            updateStatement = connection.prepareStatement("UPDATE book SET name = ?, path = ? WHERE id = ?");
+            getByIdStatement = connection.prepareStatement("SELECT * FROM book WHERE id = ?");
+        } catch (SQLException e) {
+            IO.printError("Exception while preparing statements: " + e.getMessage());
+        }
+    }
     @Override
     public List<Book> findAllById(Collection<Long> ids) {
         return null;
